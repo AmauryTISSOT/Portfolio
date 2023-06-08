f7fe58822feeacedd64a53850cf39a5968b24b7d
amaury <tissot.amaury@gmail.com>
2023-06-09 00:04:45 +0200

    Make responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,14 +9,14 @@ const Contact = () => {
       <h1 id="contact" className={`${styles.sectionHeadText}`}>
         Contact
       </h1>
-      <div className="w-full mt-20 flex flex-wrap justify-between">
+      <div className="w-full mt-10 grid grid-cols-2 gap-12 sm:gap-0 sm:grid-cols-4 justify-items-center">
         {contactData.map((data, index) => (
           <a
             key={index}
             href={data.link}
             target="_blank"
             rel="noreferrer"
-            className="w-[150px] h-[150px] flex flex-col justify-center items-center"
+            className="w-14 h-14 sm:h-[100px] sm:w-[100px] flex flex-col justify-center items-center"
           >
             <img src={data.icon} alt={data.name} className="bg-transparent" />
             <h1 className="text-white font-bold text-[24px] mt-1">
